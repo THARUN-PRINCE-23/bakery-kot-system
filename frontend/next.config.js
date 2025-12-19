6768cab69989cc46f9f5ca6b9924c401bd07e686
THARUN-PRINCE <tharun.selva.zoom@gmail.com>
2025-12-19 11:20:39 +0530

    Fix ws optional dependency build issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  distDir: ".next-dev",
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        bufferutil: false,
+        "utf-8-validate": false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
